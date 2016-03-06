849261ed86dbf4acc242481a8d2610d8d7e58cb3
Octavian Bujor <ottavio.bo@gmail.com>
2016-03-06 20:07:21 +0100

    Added background, and card images

--- src/org/unoxuno/game/MainClass.java
+++ src/org/unoxuno/game/MainClass.java
@@ -6,17 +6,24 @@ import org.newdawn.slick.AppGameContainer;
 import org.newdawn.slick.BasicGame;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
+import org.newdawn.slick.Image;
 import org.newdawn.slick.SlickException;
 
 public class MainClass extends BasicGame
 {
+
+	Image gameBG = null;
+	static int width = 768;
+	static int height = 432;
 	public MainClass(String gamename)
 	{
 		super(gamename);
 	}
 
 	@Override
-	public void init(GameContainer gc) throws SlickException {}
+	public void init(GameContainer gc) throws SlickException {
+		gameBG = new Image("res/images/main_background.jpg");
+	}
 
 	@Override
 	public void update(GameContainer gc, int i) throws SlickException {}
@@ -24,7 +31,7 @@ public class MainClass extends BasicGame
 	@Override
 	public void render(GameContainer gc, Graphics g) throws SlickException
 	{
-		g.drawString("Howdy!", 10, 10);
+		g.drawImage(gameBG.getScaledCopy(MainClass.width, MainClass.height), 0, 0);
 	}
 
 	public static void main(String[] args)
@@ -33,7 +40,8 @@ public class MainClass extends BasicGame
 		{
 			AppGameContainer appgc;
 			appgc = new AppGameContainer(new MainClass("UNOxUNO Game"));
-			appgc.setDisplayMode(640, 480, false);
+			appgc.setShowFPS(false);
+			appgc.setDisplayMode(width, height, false);
 			appgc.start();
 		}
 		catch (SlickException ex)
