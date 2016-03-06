b4f304708e0e9ef3f1a8df6e2b9432c79ac833ec
Luca Rinaldi <luca.rinaldi11@studio.unibo.it>
2016-03-06 19:08:12 +0100

    Scheletro della creazione delle stanze

--- src/ISquareRoot.java
+++
@@ -1,8 +0,0 @@
-import java.rmi.Remote;
-import java.rmi.RemoteException;
-
-public interface ISquareRoot extends Remote {
-	double calculateSquareRoot(double aNumber) throws RemoteException;
-}
-
-
