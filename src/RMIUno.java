3c6018e430e8e232b98ae04044ddab128c0fcb50
Luca Rinaldi <luca.rinaldi11@studio.unibo.it>
2016-03-06 20:08:05 +0100

    Ping

--- src/RMIUno.java
+++ src/RMIUno.java
@@ -1,21 +1,63 @@
 
 import java.net.MalformedURLException;
+import java.rmi.ConnectException;
 import java.rmi.Naming;
 import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
 import java.util.ArrayList;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class RMIUno extends UnicastRemoteObject 
 implements IUno{
 	
 	private static final long serialVersionUID = 1L;
 	String nickname;
+	int myId;
 	ArrayList<String> playersnames = null;
 	
+	private class Task extends TimerTask{
+
+		@Override
+		public void run() {
+			int nextId = (myId +1)% playersnames.size();
+			if (nextId != myId){
+				String nextname = playersnames.get(nextId);
+				try
+				{
+					IUno tempServer = 
+					(IUno) Naming.lookup("rmi://localhost/"+nextname);
+					tempServer.ping();
+				}
+				catch(ConnectException e){
+					System.out.println("Successivo non trovato, rimozione");
+					playersnames.remove(nextId);
+				}
+				catch(NotBoundException e)
+				{
+					e.printStackTrace( );
+				}
+				catch(RemoteException e)
+			  	{
+					e.printStackTrace( );
+			  	}
+				catch(MalformedURLException e)
+				{
+					e.printStackTrace( );
+				}
+			}
+		}
+		
+	}
 	 public RMIUno(String name)throws RemoteException{
 		 this.playersnames = new ArrayList<String>();
 		 this.nickname = name;
+		 this.playersnames.add(name);
+		 this.myId = 0;
+		Timer t = new Timer();
+		t.scheduleAtFixedRate(new Task(), 2000, 2000);
+
 	 }
 	 
 	 public static void main(String[] args) {
@@ -61,7 +103,8 @@ implements IUno{
 	@Override
 	public void connectReply(String name) throws RemoteException {
 		this.playersnames.add(name);
-		System.out.print(playersnames);
+		//System.out.print(playersnames);
+		System.out.println("Utente entrato in stanza: "+name);
 		try
 		  {
 			for (String n: playersnames){
@@ -87,7 +130,18 @@ implements IUno{
 	@Override
 	public void refreshUserList(ArrayList<String> users) throws RemoteException {
 		this.playersnames = users;
-		System.out.print(playersnames);
+		//System.out.print(playersnames);
+		for (int i=0; i<users.size(); i++){
+			String u = users.get(i);
+			if (u.equals(this.nickname))
+				myId = i;
+		}
+		
+	}
+
+	@Override
+	public void ping() throws RemoteException {
+		System.out.println("Pingato");
 		
 	}
 
