25ea48de12609f6fd7547e1ae97abeeaeb899990
Luca Rinaldi <luca.rinaldi11@studio.unibo.it>
2016-03-10 18:30:09 +0100

    Stato corretto: aggiunto hashmap

--- src/org/unoxuno/communication/GameState.java
+++ src/org/unoxuno/communication/GameState.java
@@ -3,54 +3,54 @@ package org.unoxuno.communication;
 import java.io.Serializable;
 import java.rmi.registry.Registry;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class GameState implements Serializable{
-	
+
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-	
+
 	private ArrayList<String> users;
-	//private ArrayList<String> domains;
-	//private ArrayList<Integer> ports;
-	private ArrayList<Registry>  registries;
 	private ArrayList<Card> deck;
 	private ArrayList<Card> discarded;
 	private boolean clockwiseSense;
-	
+	private Map<String,Registry> usersdata;
+
 	public GameState(String name, Registry r){
 		users = new ArrayList<String>();
-		registries = new ArrayList<Registry>();
-		//domains = new ArrayList<String>();
-		//ports = new ArrayList<Integer>();
+		usersdata = new HashMap<String,Registry>();
 		users.add(name);
-		registries.add(r);
-		//domains.add(dom);
-		//ports.add(port);
+		usersdata.put(name, r);
 		deck = new ArrayList<Card>();
 		discarded = new ArrayList<Card>();
 		clockwiseSense = true;
 	}
-	
+
 	public String getUsername(int id){
 		return users.get(id);
 	}
-		
+
+	public ArrayList<String> getUsernames(){
+		return users;
+	}
+
 	public int getNumberOfUsers(){
 		return users.size();
 	}
 	public int addUser(String name, Registry r){
 		users.add(name);
-		registries.add(r);
+		usersdata.put(name, r);
 		return users.size();
 	}
-	
+
 	public void removeUser(int id){
-		users.remove(id);
-		registries.remove(id);
+		String name = users.remove(id);
+		usersdata.remove(name);
 	}
-	
+
 	public int getUserId(String name){
 		for (int i=0; i<users.size(); i++){
 			String u = users.get(i);
@@ -59,54 +59,50 @@ public class GameState implements Serializable{
 		}
 		return 0;
 	}
-	
+
 	public int getNextId(int id){
-			return (id + 1) % users.size();
+		return (id + 1) % users.size();
 	}
-	
-	/*public String getDomain(int id){
-		return domains.get(id);
+
+	public Registry getRegistry(String name){
+		return usersdata.get(name);
 	}
-	
-	public int getPort(int id){
-		return ports.get(id);
-	}*/
-	
-	public Registry getRegistry(int id){
-		return registries.get(id);
+
+	public Map<String,Registry> getAllRegistries(){
+		return usersdata;
 	}
-	
+
 	public ArrayList<Card> getDeck(){
 		return deck;
 	}
-	
+
 	public boolean getSense(){
 		return clockwiseSense;
 	}
-	
+
 	public void reverseSense(){
 		clockwiseSense = !clockwiseSense;
 	}
-	
+
 	public Card getCard(){
 		int cardId = (int) (Math.random() * deck.size());
 		Card c = deck.get(cardId);
 		deck.remove(cardId);
 		return c;
 	}
-	
+
 	public int getDeckSize(){
 		return deck.size();
 	}
-	
+
 	public void discard(Card c){
 		discarded.add(c);
 	}
-	
+
 	public void refillDeck(){
 		deck.addAll(discarded);
 		discarded.clear();
 	}
-	
+
 
 }
--- src/org/unoxuno/communication/RMIUno.java
+++ src/org/unoxuno/communication/RMIUno.java
@@ -1,29 +1,26 @@
 package org.unoxuno.communication;
 
-import java.net.MalformedURLException;
 import java.rmi.ConnectException;
-import java.rmi.Naming;
 import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
-import java.rmi.server.RemoteServer;
-import java.rmi.server.ServerNotActiveException;
 import java.rmi.server.UnicastRemoteObject;
+import java.util.Map;
 import java.util.Timer;
 import java.util.TimerTask;
 
 public class RMIUno extends UnicastRemoteObject 
 implements IUno{
-	
+
 	private static final long serialVersionUID = 1L;
 	String nickname;
 	int myId;
 	GameState state;
 	boolean token = false;
 	Registry localRegistry;
-	
-	/*private class Task extends TimerTask{
+
+	private class Task extends TimerTask{
 
 		@Override
 		public void run() {
@@ -34,11 +31,11 @@ implements IUno{
 				int nextId = state.getNextId(myId);
 				if (nextId != myId){
 					String nextname = state.getUsername(nextId);
-					String nextdomain = state.getDomain(nextId);
+					//String nextdomain = state.getDomain(nextId);
 					try
 					{
 						IUno tempServer = 
-								(IUno) Naming.lookup("rmi://"+nextdomain+"/"+nextname);
+								(IUno) state.getRegistry(nextname).lookup(nextname);
 						tempServer.ping(nickname);
 					}
 					catch(ConnectException e){
@@ -58,78 +55,71 @@ implements IUno{
 					{
 						e.printStackTrace( );
 					}
-					catch(MalformedURLException e)
-					{
-						e.printStackTrace( );
-					}
 				}
 			}
 			if (changed){
 				try
-			  {
-				for (int i=0; i<state.getNumberOfUsers(); i++){
-					if (i != myId){
-						IUno tempServer = 
-								(IUno) Naming.lookup("rmi://"+state.getDomain(i)+"/"+state.getUsername(i));
-						tempServer.refreshState(state);
+				{
+					Map<String,Registry> reg = state.getAllRegistries();
+					for (String regname : state.getUsernames()){
+						if (regname != nickname){
+							IUno tempServer = 
+									(IUno) reg.get(regname).lookup(regname);
+							tempServer.refreshState(state);
+						}
 					}
 				}
-			  }
-			  catch(NotBoundException e)
-			  {
-			   e.printStackTrace( );
-			  }
-			  catch(RemoteException e)
-			  {
-			   e.printStackTrace( );
-			  }
-			  catch(MalformedURLException e)
-			  {
-			   e.printStackTrace( );
-			  }
+				catch(NotBoundException e)
+				{
+					e.printStackTrace( );
+				}
+				catch(RemoteException e)
+				{
+					e.printStackTrace( );
+				}
+			}
 		}
-		}
-		
-	}
-	*/
-	 public RMIUno(String name,int port)throws RemoteException{
-		 this.nickname = name;
-		 this.myId = 0;
-		 localRegistry = LocateRegistry.createRegistry(port);
-		 localRegistry.rebind(name, this);
-		 state = new GameState(name, localRegistry);
-		 System.out.println("Binding eseguito su "+name+" in porta "+port);
-			 //Naming.rebind("//"+dom+"/"+name,this);
-		 //Timer t = new Timer();
-		 //t.scheduleAtFixedRate(new Task(), 2000, 2000);
 
-	 }
-	 
-	 public static void main(String[] args) throws RemoteException {
-		 RMIUno server = new RMIUno(args[0],Integer.parseInt(args[1]));
-		 if (args.length == 5)
-				  server.connectSend(args[0],Integer.parseInt(args[4]),args[2],args[3]);
-		 		  server.token = true;
-			  
-	 }
-	 
-	 private void connectSend(String myname, int serverport, String servername, String serverdom){
-		 try
-		  {
-			 System.out.println("Tentativo di connessione da parte di "+myname+" su "+servername+" con porta "+serverport+" all'indirizzo "+serverdom);
-			 Registry tempRegistry = LocateRegistry.getRegistry(serverdom, serverport);
-			 IUno tempServer = (IUno) tempRegistry.lookup(servername);
-		     tempServer.connectReply(myname,localRegistry);
-		  }
-		  catch(NotBoundException e)
-		  {
-		   e.printStackTrace( );
-		  }
-		  catch(RemoteException e)
-		  {
-		   e.printStackTrace( );
-		  }
-		 }
+	}
+
+	public RMIUno(String name,int port)throws RemoteException{
+		this.nickname = name;
+		this.myId = 0;
+		localRegistry = LocateRegistry.createRegistry(port);
+		localRegistry.rebind(name, this);
+		state = new GameState(name, localRegistry);
+		System.out.println("Binding eseguito su "+name+" in porta "+port);
+		//Naming.rebind("//"+dom+"/"+name,this);
+		Timer t = new Timer();
+		t.scheduleAtFixedRate(new Task(), 2000, 2000);
+
+	}
+
+	public static void main(String[] args) throws RemoteException {
+		RMIUno server = new RMIUno(args[0],Integer.parseInt(args[1]));
+		if (args.length == 5)
+			server.connectSend(args[0],Integer.parseInt(args[4]),args[2],args[3]);
+		server.token = true;
+
+	}
+
+	private void connectSend(String myname, int serverport, String servername, String serverdom){
+		try
+		{
+			System.out.println("Tentativo di connessione da parte di "+myname+" su "+servername+" con porta "+serverport+" all'indirizzo "+serverdom);
+			Registry tempRegistry = LocateRegistry.getRegistry(serverdom, serverport);
+			IUno tempServer = (IUno) tempRegistry.lookup(servername);
+			tempServer.connectReply(myname,localRegistry);
+		}
+		catch(NotBoundException e)
+		{
+			e.printStackTrace( );
+		}
+		catch(RemoteException e)
+		{
+			e.printStackTrace( );
+		}
+	}
 
 	@Override
 	public void connectReply(String name, Registry r) throws RemoteException {
@@ -138,24 +128,24 @@ implements IUno{
 		//System.out.print(playersnames);
 		System.out.println("Utente entrato in stanza: "+name);
 		try
-		  {
-			for (int i=0; i<state.getNumberOfUsers(); i++){
-				if (i != myId){
-					Registry tempRegistry = state.getRegistry(i);
+		{
+			Map<String,Registry> reg = state.getAllRegistries();
+			for (String regname : state.getUsernames()){
+				if (regname != nickname){
 					IUno tempServer = 
-							(IUno) tempRegistry.lookup(state.getUsername(i));
+							(IUno) reg.get(regname).lookup(regname);
 					tempServer.refreshState(state);
 				}
 			}
-		  }
-		  catch(NotBoundException e)
-		  {
-		   e.printStackTrace( );
-		  }
-		  catch(RemoteException e)
-		  {
-		   e.printStackTrace( );
-		  }
+		}
+		catch(NotBoundException e)
+		{
+			e.printStackTrace( );
+		}
+		catch(RemoteException e)
+		{
+			e.printStackTrace( );
+		}
 	}
 
 	@Override
@@ -168,7 +158,7 @@ implements IUno{
 	@Override
 	public void ping(String name) throws RemoteException {
 		System.out.println("Pingato da "+name);
-		
+
 	}
 
 }
